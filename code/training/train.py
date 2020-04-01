be469b80d521f9944c10b36e6ee32ec932aa8c12
Eugene Fedorenko <efedorenko@Eugenes-MacBook-Pro.local>
2020-04-01 11:26:51 -0700

    rebuild image

--- code/training/train.py
+++ code/training/train.py
@@ -81,10 +81,9 @@ def run(
 
   # training data
   train_data, train_labels = zip(*train)
-  print(train_labels) 
+  
   train_ds = Dataset.zip((Dataset.from_tensor_slices(list(train_data)),
-                          Dataset.from_tensor_slices(list(train_data)),
-                          img_size))
+                          Dataset.from_tensor_slices(list(train_labels))))
 
   train_ds = train_ds.map(map_func=process_image,
                           num_parallel_calls=5)
