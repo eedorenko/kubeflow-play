f7a532e6ece0c30ee140c4e290a6a8f2f0dfbca4
Eugene Fedorenko <efedorenko@DESKTOP-UCKQHTN.redmond.corp.microsoft.com>
2020-04-03 16:08:06 -0700

    Publish pieplien fix

--- code/publish_pipeline.py
+++ code/publish_pipeline.py
@@ -1,12 +1,20 @@
 import kfp
 import os
-
-
-host = "http://23.101.114.95/pipeline"
-namespace = "play"
-pipeline_file_path = "pipeline.py.tar.gz"
-pipeline_name = "mexicanfood"
+import kfp_server_api.api_client
+import kfp_server_api.models.api_pipeline_version
+import kfp_server_api.models
+host = "http://localhost:8080/pipeline"
+namespace = "kubeflow"
+pipeline_file_path = "/Users/efedorenko/work/kfp/pipeline.yaml"
+pipeline_name = "Banditos Tacos and Burritos"
 
 client = kfp.Client(host=host, namespace=namespace)
 pipeline_file = os.path.join(pipeline_file_path)
 pipeline = client.pipeline_uploads.upload_pipeline(pipeline_file, name=pipeline_name)  # noqa: E501
+# pipeline_version = client.pipeline_uploads.upload_pipeline_version(pipeline_file,
+#                                                                    name="Version1",
+#                                                                    pipelineid=pipeline.id)
+                                                                   
+
+
+
