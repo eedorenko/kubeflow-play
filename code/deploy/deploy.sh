0d949473a9fa07352a8e50eb9a9397eb49ba2abb
Eugene Fedorenko <eugene.fedor@gmail.com>
2020-04-08 17:01:06 -0700

    deploy

--- code/deploy/deploy.sh
+++ code/deploy/deploy.sh
@@ -20,7 +20,12 @@ done
 az login --service-principal --username ${SERVICE_PRINCIPAL_ID} --password ${SERVICE_PRINCIPAL_PASSWORD} -t $TENANT_ID
 # az ml model deploy -n $MODEL_NAME -m ${MODEL}:1 --ic $INFERENCE_CONFIG --pi ${BASE_PATH}/myprofileresult.json --dc $DEPLOYMENTCONFIG -w $WORKSPACE -g $RESOURCE_GROUP --overwrite -v
 
-model_id=$(az ml model list --model-name ${MODEL} --workspace-name $WORKSPACE -g $RESOURCE_GROUP --tag run_id=$RUN_ID | jq '.[0] | .id') 
+# model_id=$(az ml model list --model-name $MODEL --workspace-name $WORKSPACE -g $RESOURCE_GROUP --tag run_id=$RUN_ID | jq '.[0] | .id') 
+echo $MODEL
+echo $WORKSPACE
+echo $RESOURCE_GROUP
+echo $SUBSCRIPTION_ID
+model_id=$(az ml model list --model-name $MODEL --workspace-name $WORKSPACE -g $RESOURCE_GROUP --subscription-id $SUBSCRIPTION_ID | jq '.[0] | .id') 
 echo $model_id
 
 az ml model deploy -n "mexicanfood" -m $model_id --ic $INFERENCE_CONFIG  --dc $DEPLOYMENTCONFIG -w $WORKSPACE -g $RESOURCE_GROUP --overwrite -v
