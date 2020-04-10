72fbbf58d88a097ae3014aa09cbb2fd478f382f4
Eugene Fedorenko <eugene.fedor@gmail.com>
2020-04-09 18:34:30 -0700

    azure secrets

--- code/pipeline.py
+++ code/pipeline.py
@@ -10,22 +10,17 @@ from kfp.azure import use_azure_secret
     description='Simple TF CNN'
 )
 def tacosandburritos_train(
-    tenant_id,
-    service_principal_id,
-    service_principal_password,
-    subscription_id,
     resource_group,
     workspace
 ):
     """Pipeline steps"""
 
     persistent_volume_path = '/mnt/azure'
-    # data_download = 'https://aiadvocate.blob.core.windows.net/public/tacodata.zip'  # noqa: E501
+    data_download = 'https://aiadvocate.blob.core.windows.net/public/tacodata.zip'  # noqa: E501
     epochs = 2
     batch = 32
     learning_rate = 0.0001
     model_name = 'tacosandburritos'
-    # profile_name = 'tacoprofile'
     operations = {}
     image_size = 160
     training_folder = 'train'
@@ -35,31 +30,19 @@ def tacosandburritos_train(
 
     # preprocess data
 
-    op = dsl.ContainerOp(
+    operations['preprocess'] = dsl.ContainerOp(
         name='preprocess',
-        image=image_repo_name + '/deploy:latest',
-        command=['sh'],
+        image=image_repo_name + '/preprocess:latest',
+        command=['python'],
         arguments=[
-            '-c',
-            'echo $AZ_TENANT_ID'
+            '/scripts/data.py',
+            '--base_path', persistent_volume_path,
+            '--data', training_folder,
+            '--target', training_dataset,
+            '--img_size', image_size,
+            '--zipfile', data_download
         ]
     )
-    op.apply(use_azure_secret())
-    operations['preprocess'] = op
-
-    # operations['preprocess'] = dsl.ContainerOp(
-    #     name='preprocess',
-    #     image=image_repo_name + '/preprocess:latest',
-    #     command=['python'],
-    #     arguments=[
-    #         '/scripts/data.py',
-    #         '--base_path', persistent_volume_path,
-    #         '--data', training_folder,
-    #         '--target', training_dataset,
-    #         '--img_size', image_size,
-    #         '--zipfile', data_download
-    #     ]
-    # )
 
     # train
     operations['training'] = dsl.ContainerOp(
@@ -90,15 +73,11 @@ def tacosandburritos_train(
             '--base_path', persistent_volume_path,
             '--model', 'latest.h5',
             '--model_name', model_name,
-            '--tenant_id', tenant_id,
-            '--service_principal_id', service_principal_id,
-            '--service_principal_password', service_principal_password,
-            '--subscription_id', subscription_id,
             '--resource_group', resource_group,
             '--workspace', workspace,
             '--run_id', dsl.RUN_ID_PLACEHOLDER
         ]
-    )
+    ).apply(use_azure_secret())
     operations['register'].after(operations['training'])
 
     operations['deploy'] = dsl.ContainerOp(
@@ -111,16 +90,12 @@ def tacosandburritos_train(
             '-m', model_name,
             '-i', '/scripts/inferenceconfig.json',
             '-d', '/scripts/deploymentconfig.json',
-            '-t', tenant_id,
             '-r', resource_group,
             '-w', workspace,
-            '-s', service_principal_id,
-            '-p', service_principal_password,
-            '-u', subscription_id,
             '-b', persistent_volume_path,
             '-x', dsl.RUN_ID_PLACEHOLDER
         ]
-    )
+    ).apply(use_azure_secret())
     operations['deploy'].after(operations['register'])
 
     for _, op_1 in operations.items():
